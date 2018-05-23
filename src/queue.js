4786330f5fd245d7ac258aa6bdfa5ce901b69d01
tymiurge <tmrlabas@gmail.com>
2018-05-23 14:02:00 +0300

    Queue: size()

--- src/queue.js
+++ src/queue.js
@@ -1,1 +1,32 @@
 // https://eli.thegreenplace.net/2013/10/22/classical-inheritance-in-javascript-es5
+
+// rear - at the back
+// enqueue - put to a queue
+// dequeue - remove from a queue
+
+
+// Double-ended queue
+// Circular queue
+// priority queue
+// 
+function Queue() {
+    this.queue = []
+}
+
+Queue.prototype.put = function(node) {
+    this.queue.push(node)
+}
+
+Queue.prototype.size = function() {
+    return this.queue.length
+}
+
+Queue.prototype.get = function() {
+    let node = this.queue[0]
+    this.queue = this.queue.slice(1, this.queue.length - 1)
+    return node
+}
+
+Queue.prototype.constructor = Queue
+
+export default Queue
---
+++ tests/queue-test.js
@@ -0,0 +1,18 @@
+import expect from 'expect'
+import Queue from './../src/queue'
+
+describe('Module template', () => {
+    it('test whether size is increased at putting a node', () => {
+        let queue = new Queue()
+        queue.put('node')
+        expect(queue.size()).toBe(1)
+    })
+
+    it('test whether size is decreased at getting a node', () => {
+        let queue = new Queue()
+        queue.put('node')
+        queue.get()
+        expect(queue.size()).toBe(0)
+    })
+})
+
