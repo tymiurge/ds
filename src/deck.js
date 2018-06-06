1374c2e3de1693bdba8e59cd771fbdf953928e53
tymiurge <tmrlabas@gmail.com>
2018-06-06 22:33:00 +0300

    prototype inheritance for queue and dequeue - initiating

--- src/deck.js
+++ src/deck.js
@@ -6,14 +6,10 @@ Deck.prototype.size = function() {
     return data.length
 }
 
-
 Deck.prototype.dequeue = function() {
     let node = data[0]
     this.data = data.slice(1, data.length)
     return node
 }
-
-
-
 
 export default Deck
