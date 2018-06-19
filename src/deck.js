204e1a171131ed5c6b9e4f4d7b1825ac2054d01f
tymiurge <tmrlabas@gmail.com>
2018-06-19 14:58:47 +0300

    fixing sharing deck data across different instances of queue

--- src/deck.js
+++ src/deck.js
@@ -1,19 +1,20 @@
-let data = []
 
-function Deck() {}
+function Deck() {
+    let data = []
 
-Deck.prototype.size = function() {
-    return data.length
-}
+    this.size =  function() {
+        return data.length
+    }
 
-Deck.prototype.dequeue = function() {
-    let node = data[0]
-    data = data.slice(1, data.length)
-    return node
-}
+    this.dequeue = function() {
+        let node = data[0]
+        data = data.slice(1, data.length)
+        return node
+    }
 
-Deck.prototype.enqueue = function(node) {
-    data.push(node)
+    this.enqueue = function(node) {
+        data.push(node)
+    }
 }
 
 export default Deck
