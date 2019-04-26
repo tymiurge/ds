ca1f70491975b62cdc28c021af6071ddb71b4d86
tymiurge <tmrlabas@gmail.com>
2019-04-26 21:22:49 +0300

    tasks on stack added

--- src/stack/object-based-stack.js
+++ src/stack/object-based-stack.js
@@ -7,10 +7,64 @@
  * the more time it takes to. 
  * also an array is an ordered set of the elements, and
  * to keep the elements in order it would need more
- * space in the memory as well (mostly becase array is 
- * inherited at least from Iterable)
+ * space in the memory as well (mostly becase array
+ * implements at least Iterable interface).
+ * 
+ * so here object-based stack is represented.
+ * 
+ * @see src/stack/stack.js
+ * @see src/stack/array-based-stack.js
  * 
  */
 class Stack {
 
+  constructor() {
+    this.count = 0;
+    this.items = {};
+  }
+
+  push(element) {
+    this.items[this.count] = element;
+    this.count = this.count + 1;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.count === 0;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    this.count = this.count - 1;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    // or, with ES6
+    // const { [this.count]: result, ...rest} = this.items;
+    // this.items = rest;
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+  }
+
+  toString() {
+    if (this.isEmpty) {
+      return '';
+    }
+    return this.items.join(',')
+  }
 }
---
+++ src/stack/tasks.md
@@ -0,0 +1,4 @@
+## Stack
+
+### undo
+In general stack DS can be used for backtracking problems to remember tasks or paths visited, and to undo actions. 
